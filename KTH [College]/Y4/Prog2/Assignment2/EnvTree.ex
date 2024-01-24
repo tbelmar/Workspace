549997cd19252e8a417c7b9bbf5697f121caa39e
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2024-01-24 20:18:18 +0100

    implemented tree version of Dictionary

---
+++ KTH [College]/Y4/Prog2/Assignment2/EnvTree.ex
@@ -0,0 +1,50 @@
+defmodule EnvTree do
+  def add(nil, key, value) do
+    {:node, key, value, nil, nil}
+  end
+
+  def add({:node, key, _, left, right}, key, value) do
+    {:node, key, value, left, right}
+  end
+
+  def add({:node, k, v, left, right}, key, value) when key < k do
+     {:node, k, v, add(left, key, value), right}
+  end
+
+  def add({:node, k, v, left, right}, key, value) do
+    {:node, k, v, left, add(right, key, value)}
+  end
+
+  def remove(nil, _) do
+    nil
+  end
+
+  def remove({:node, key, _, nil, right}, key) do
+    right
+  end
+
+  def remove({:node, key, _, left, nil}, key) do
+    left
+  end
+
+  def remove({:node, key, _, left, right}, key) do
+    {k, v} = leftmost(right)
+    {:node, k, v, left, remove(right, key)}
+  end
+
+  def remove({:node, k, v, left, right}, key) when key < k do
+    {:node, k, v, remove(left, key), right}
+  end
+
+  def remove({:node, k, v, left, right}, key) do
+    {:node, k, v, left, remove(right, key)}
+  end
+
+  def leftmost({:node, key, value, nil, _}) do
+    {key, value}
+  end
+
+  def leftmost({:node, k, v, left, right}) do
+    leftmost(left)
+  end
+end
