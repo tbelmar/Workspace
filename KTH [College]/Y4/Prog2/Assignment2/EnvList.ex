5e2a4212eca83cbd7fa0a8758e1d36adadce05bb
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2024-01-24 18:37:56 +0100

    finished part 1 of assignment 2

---
+++ KTH [College]/Y4/Prog2/Assignment2/EnvList.ex
@@ -0,0 +1,47 @@
+defmodule EnvList do
+  def new() do
+    []
+  end
+
+  def add([], key, value) do
+    [{key, value}]
+  end
+
+  def add([{key, _} | tail], key, value) do
+    [{key, value} | tail]
+  end
+
+  def add([head | tail], key, value) do
+    [head | add(tail, key, value)]
+  end
+
+  def lookup([], _) do
+    nil
+  end
+
+  def lookup([{key, value} | _], key) do
+    {key, value}
+  end
+
+  def lookup([_ | tail], key) do
+    lookup(tail, key)
+  end
+
+  def remove([], _) do
+    []
+  end
+
+  def remove([{key, _} | tail], key) do
+    tail
+  end
+
+  def remove([head | tail], key) do
+    [head | remove(tail, key)]
+  end
+end
+
+map = [{:apple, 1}, {:pear, 2}, {:orange, 3}]
+
+IO.inspect(EnvList.add(map, :apple, 4))
+IO.inspect(EnvList.remove(map, :apple))
+IO.inspect(EnvList.lookup(map, :apple))
